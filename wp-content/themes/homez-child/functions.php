3e286df94f43cb8164d9b00a07968c4d94c5cbb3
Regan McGregor <me@reganmcgregor.com.au>
2024-06-12 21:42:27 +1000

    Add property type label

--- wp-content/themes/homez-child/functions.php
+++ wp-content/themes/homez-child/functions.php
@@ -5,3 +5,32 @@ function homez_child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'homez_child_enqueue_styles', 200 );
+
+function homez_property_display_type_label($post, $echo = true, $color = true) {
+    $types = get_the_terms( $post->ID, 'property_type' );
+    ob_start();
+    if ( $types ) {
+        foreach ($types as $term) {
+            $text_color = get_term_meta( $term->term_id, 'text_color', true );
+            $bg_color = get_term_meta( $term->term_id, 'bg_color', true );
+            $style = '';
+            if ( $color ) {
+                if ( $bg_color ) {
+                    $style .= 'background: '.$bg_color.';';
+                }
+                if ( $text_color ) {
+                    $style .= 'color: '.$text_color.';';
+                }
+            }
+            ?>
+                <a class="type-property-label" href="<?php echo esc_url(get_term_link($term)); ?>" style="<?php echo esc_attr($style); ?>"><?php echo esc_html($term->name); ?></a>
+            <?php
+        }
+    }
+    $output = ob_get_clean();
+    if ( $echo ) {
+        echo trim($output);
+    } else {
+        return $output;
+    }
+}
