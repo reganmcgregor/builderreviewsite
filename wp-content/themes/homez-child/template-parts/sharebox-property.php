b0bc405e2c684bd903e3d374a21e45f5ae80158b
Regan McGregor <me@reganmcgregor.com.au>
2024-06-12 21:39:43 +1000

    Copied Sharebox Template For Customisation

---
+++ wp-content/themes/homez-child/template-parts/sharebox-property.php
@@ -0,0 +1,38 @@
+<?php
+global $post;
+wp_enqueue_script('addthis');
+if ( !homez_get_config('show_property_social_share', true) ) {
+	return;
+}
+?>
+<div class="social-property">
+	<a href="javascript:void(0);" class="btn-add-social">
+		<i class="flaticon-share-1"></i>
+	</a>
+	<div class="bo-social-icons">
+		<?php if ( homez_get_config('facebook_share', 1) ): ?>
+
+ 			<a class="bo-social-facebook" data-toggle="tooltip" data-original-title="Facebook" href="http://www.facebook.com/sharer.php?s=100&u=<?php the_permalink(); ?>" target="_blank" title="<?php echo esc_html__('Share on facebook', 'homez'); ?>">
+				<i class="fab fa-facebook-f"></i>
+			</a>
+
+		<?php endif; ?>
+		<?php if ( homez_get_config('twitter_share', 1) ): ?>
+ 			<a class="bo-social-twitter" data-toggle="tooltip" data-original-title="Twitter" href="https://twitter.com/intent/tweet?url=<?php the_permalink(); ?>" target="_blank" title="<?php echo esc_html__('Share on Twitter', 'homez'); ?>">
+				<i class="fab fa-twitter"></i>
+			</a>
+		<?php endif; ?>
+		<?php if ( homez_get_config('linkedin_share', 1) ): ?>
+ 			<a class="bo-social-linkedin"  data-toggle="tooltip" data-original-title="LinkedIn" href="http://linkedin.com/shareArticle?mini=true&amp;url=<?php the_permalink(); ?>&amp;title=<?php the_title(); ?>" target="_blank" title="<?php echo esc_html__('Share on LinkedIn', 'homez'); ?>">
+				<i class="fab fa-linkedin-in"></i>
+			</a>
+		<?php endif; ?>
+		
+		<?php if ( homez_get_config('pinterest_share', 1) ): ?>
+ 			<a class="bo-social-pinterest" data-toggle="tooltip" data-original-title="Pinterest" href="http://pinterest.com/pin/create/button/?url=<?php echo urlencode(get_permalink()); ?>&amp;media=<?php echo urlencode($img); ?>" target="_blank" title="<?php echo esc_html__('Share on Pinterest', 'homez'); ?>">
+				<i class="fab fa-pinterest-p"></i>
+			</a>
+		<?php endif; ?>
+
+	</div>
+</div>
