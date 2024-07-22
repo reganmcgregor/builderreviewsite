9e438739be088b776f8f635d3f27157f8d02e81b
Regan McGregor <me@reganmcgregor.com.au>
2024-07-23 08:21:14 +1000

    Update Build Locations Layout

--- wp-content/themes/homez-child/template-properties/single-property/build-locations.php
+++ wp-content/themes/homez-child/template-properties/single-property/build-locations.php
@@ -14,16 +14,53 @@ $map_service = wp_realestate_get_option('map_service', 'mapbox');
 
 $build_locations = get_the_terms($post->ID, 'property_location');
 
+function print_taxonomy_with_parent($terms){
+
+    if ( ! is_array( $terms ) || empty( $terms ) ) {
+        return false;
+    }
+
+    $parent_terms = array();
+
+    // get only parents
+    foreach ( $terms as $term ) {
+        if ($term->parent === 0) {
+            $term->child = Array();
+            $parent_terms[] = $term;
+        }
+    }
+
+    // compare and nested
+    foreach ( $terms as $term ) {
+        if ($term->parent != 0) {
+            foreach ($parent_terms as $key => $value) {
+                if ($term->parent === $value->term_id) {
+                    $parent_terms[$key]->child[] = $term;
+                }
+            }
+        }
+    }
+
+    // output results
+    foreach ( $parent_terms as $term ) {
+
+        //parent term
+		echo '<ul class="list list-detail d-flex flex-wrap">';
+        echo '<li class="d-flex align-items-center" style="width: 100%;"><div class="text">'.$term->name.'</div>';
+
+        if ($term->child) {
+            foreach ( $term->child as $child ) {
+                echo '<li class="d-flex align-items-center text">'.$child->name.'</li>';                
+            }          
+        }
+		echo '</li>';
+        echo '</ul>';
+    }
+}
 ?>
 <?php if (!empty($build_locations)) : ?>
 	<div class="property-section">
 		<h3 class="title"><?php esc_html_e('Build Locations', 'homez'); ?></h3>
-		<ul class="list list-detail d-flex flex-wrap">
-			<?php foreach ($build_locations as $build_location) : ?>
-				<li class="d-flex align-items-center">
-					<?php echo esc_html($build_location->name); ?>
-				</li>
-			<?php endforeach; ?>
-		</ul>
+		<?php print_taxonomy_with_parent($build_locations); ?>
 	</div>
 <?php endif; ?>
