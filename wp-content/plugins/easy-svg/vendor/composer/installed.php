1147c39dba0d828a26cd8f065d43c01b6fb3087c
Regan McGregor <me@reganmcgregor.com.au>
2024-08-11 22:02:50 +1000

    Add easy SVG

---
+++ wp-content/plugins/easy-svg/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'benjaminzekavica/easy-svg',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '301f09c95380aa845993ad65c72ee08da8e938d7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'benjaminzekavica/easy-svg' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '301f09c95380aa845993ad65c72ee08da8e938d7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'enshrined/svg-sanitize' => array(
+            'pretty_version' => '0.19.0',
+            'version' => '0.19.0.0',
+            'reference' => 'e95cd17be68e45f523cbfb0fe50cdd891b0cf20e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../enshrined/svg-sanitize',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
